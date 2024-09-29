9bbffc6375c66b1ecfac321b2386b54727e96a2a
dani7 <danis.004@mail.ru>
2024-09-29 23:20:13 +0400

    Верстка компонента header

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -5,7 +5,22 @@ const Header = () => {
     return (
         <header className={styles.header}>
             <div className="container">
-                1
+                <div className={styles.wrapper}>
+                    <div className={styles.nav}>
+                        <a href="#" className={styles.menu}>
+                            <img src="/assets/images/menu.png" alt="" />
+                        </a>
+                        <a href="#" className={styles.logo}>
+                            <img src="/logo.png" alt="" />
+                        </a>
+                    </div>
+                    <div className={styles.user}>
+                        <a href="#" className={styles.plus}>
+                            <img src="/assets/images/plus.png" alt="" />
+                        </a>
+                        <div className={styles.user_logo}></div>
+                    </div>
+                </div>
             </div>
         </header>
     )
