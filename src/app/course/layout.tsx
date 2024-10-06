613b2603c4ac8a06af6849490734c7c495b15d60
dani7 <danis.004@mail.ru>
2024-10-07 00:03:07 +0400

    Исправил баги

--- src/app/course/layout.tsx
+++ src/app/course/layout.tsx
@@ -2,7 +2,7 @@ import Course_header from "../../../components/shared/course_header";
 
 export default function CourseLayout({children,}: Readonly<{children: React.ReactNode;}>) {
     return (
-        <div>
+        <div className="main">
          <Course_header/>
          {children}
         </div>
