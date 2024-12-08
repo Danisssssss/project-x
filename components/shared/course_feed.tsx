e72ef0fa2ccdf9dcf18275d2a44d32ac0c0ed732
dani7 <danis.004@mail.ru>
2024-12-08 20:05:40 +0400

    Исправление багов

--- components/shared/course_feed.tsx
+++ components/shared/course_feed.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./course_feed.module.css";
-import Image from "next/image";
+// import Image from "next/image";
 
 const Course_feed = () => {
     return (
@@ -8,13 +8,13 @@ const Course_feed = () => {
             <div className={styles.banner}>
                 <div className={styles.title}>Название курса</div>
             </div>
-            <div className={styles.copy_block}>
+            {/* <div className={styles.copy_block}>
                 <div className={styles.code_title}>Код курса</div>
                 <div className={styles.code_element}>
                     <div className={styles.code}>xxxxxx</div>
                     <Image className={styles.btn_copy} src="/assets/images/copy.svg" alt="" width={19} height={19} />
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 };
